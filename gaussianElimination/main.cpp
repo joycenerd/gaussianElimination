8cdccd336d38189ce7cf287d2bc34fb4beaa1d5f
joycenerd <chinjoyce30@gmail.com>
2018-06-05 12:38:56 +0800

    Initial Commit

---
+++ gaussianElimination/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gaussianElimination
+//
+//  Created by Joyce Chin on 2018/6/5.
+//  Copyright © 2018年 Joyce Chin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
