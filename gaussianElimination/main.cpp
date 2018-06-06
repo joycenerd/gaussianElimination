1ccc2e2d9d51c0abcaf04959acefac6fc635620a
joycenerd <chinjoyce30@gmail.com>
2018-06-06 13:30:54 +0800

    finish file IO

--- gaussianElimination/main.cpp
+++ gaussianElimination/main.cpp
@@ -8,13 +8,31 @@
 
 #include <cstdio>
 #include <cmath>
+#include <cassert>
 #define N 120
 #define DEBUG 0
 using namespace std;
 
+FILE *outputFile;
 float augmentMatrix[N][N],determinant;
-int n;
+int n,cases;
 int flag,swapTimes;
+
+int checkValid(float x[]){
+    float ans=0;
+    int check=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            ans+=augmentMatrix[i][j]*x[j];
+        }
+        if(abs(ans-augmentMatrix[i][n])>0.0001){
+            check=0;
+            break;
+        }
+    }
+    if(check) return 1;
+    else return -1;
+}
 
 
 void swapRow(int row){
@@ -54,6 +72,9 @@ void toOne(int row){
 }
 
 void gaussianElimination(){
+    /*outputFile=fopen("/Users/joycechin/Desktop/EX7/gaussianElimination/gaussianElimination/output.out","w");
+    assert(outputFile!=NULL);*/
+    fprintf(outputFile,"Case %d:\n",cases);
     int row=0;
     int i,j;
     float divisor;
@@ -77,7 +98,7 @@ void gaussianElimination(){
             }
         }
         else{
-            printf("The determinant is 0\nThis equation is linear dependence\n");
+            fprintf(outputFile,"The determinant is 0\nThis equation is linear dependence\n");
             break;
         }
 #if DEBUG //== 1
@@ -94,23 +115,45 @@ void gaussianElimination(){
     if(row==n){
         for(i=0;i<n;i++) x[i]=augmentMatrix[i][n];
         if(swapTimes%2==1) determinant=-determinant;
-        printf("The determinant is %f\n",determinant);
-        printf("The solution vector is: ");
-        for(i=0;i<n;i++) printf("%f ",x[i]);
-        printf("\n");
+        fprintf(outputFile,"The determinant is %f\n",determinant);
+        fprintf(outputFile,"The solution vector is: ");
+        for(i=0;i<n;i++) fprintf(outputFile,"%f ",x[i]);
+        fprintf(outputFile,"\n");
+        int valid=checkValid(x);
+        if(valid) fprintf(outputFile,"This is a valid solution\n");
+        else fprintf(outputFile,"This is an invalid solution\n");
     }
+    fprintf(outputFile,"======================================================\n");
 }
 
 int main()
 {
+    FILE *inputFile;
+    inputFile=fopen("/Users/joycechin/Desktop/EX7/gaussianElimination/ex7_sample.in","r");
+    assert(inputFile!=NULL);
+    outputFile=fopen("/Users/joycechin/Desktop/EX7/gaussianElimination/gaussianElimination/output.out","w");
+    assert(outputFile!=NULL);
+    float coefficient[N][N];
+    float b[N];
+    int i,j;
+    cases=0;
     while(1){
+        fscanf(inputFile,"%d",&n);
+        if(n==0) break;
+        for(i=0;i<n;i++) for(j=0;j<n;j++) fscanf(inputFile,"%f",&coefficient[i][j]);
+        for(i=0;i<n;i++) fscanf(inputFile,"%f",&b[i]);
+        for(i=0;i<n;i++) for(j=0;j<n;j++) augmentMatrix[i][j]=coefficient[i][j];
+        for(i=0;i<n;i++) augmentMatrix[i][n]=b[i];
+        gaussianElimination();
+        cases++;
+    }
+    fclose(inputFile);
+    fclose(outputFile);
+    /*while(1){
         scanf("%d",&n);
         if(n==0) break;
-        int i,j;
-        float coefficient[N][N];
         for(i=0;i<n;i++) for(j=0;j<n;j++) scanf("%f",&coefficient[i][j]);
-        int b[N];
-        for(i=0;i<n;i++) scanf("%d",&b[i]);
+        for(i=0;i<n;i++) scanf("%f",&b[i]);
         for(i=0;i<n;i++) for(j=0;j<n;j++) augmentMatrix[i][j]=coefficient[i][j];
         for(i=0;i<n;i++) augmentMatrix[i][n]=b[i];
 #if DEBUG==1
@@ -122,6 +165,6 @@ int main()
         }
 #endif
         gaussianElimination();
-    }
+    }*/
     return 0;
 }
